79e117e30f969430f02c9c213c76544fac64741d
Cayki Santos Gondim <131631975+scayki@users.noreply.github.com>
2024-10-12 23:06:09 -0300

    Adiciona script start no package.json

--- app.js
+++ app.js
@@ -1,70 +1,60 @@
 // npm install express express-handlebars body-parser sequelize sqlite3
 
-const express = require('express')
+const express = require('express');
 const { engine } = require('express-handlebars'); // novo jeito de importra handlebars
+const path = require('path');
+const app = express();
+const db = require('./DB/connection.js');
+const bodyParser = require('body-parser');
+const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-const path = require('path')
-const app = express()
-const db = require('./DB/connection.js')
-const bodyParser = require('body-parser')
-const Job = require('./models/Job')
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
+// Use a porta fornecida pelo Heroku ou a 3000 para desenvolvimento local
+const PORT = process.env.PORT || 3000;
 
-const porta = 3000
+// bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//bodyparser
-app.use(bodyParser.urlencoded({extended:false}))
+// handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', engine({ defaultLayout: 'main' })); // novo uso das handlebars
+app.set('view engine', 'handlebars');
 
-//handlebars
-app.set('views',path.join(__dirname, 'views'))
-app.engine('handlebars', engine({ defaultLayout: 'main' })); //novo uso das handlebars
-app.set('view engine','handlebars')
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// db connection
+db.authenticate()
+  .then(() => console.log("Conectou ao banco com sucesso"))
+  .catch((err) => console.log("Ocorreu um erro ao conectar ", err));
 
-app.listen(porta, ()=> console.log(`o Express está rodando na porta ${porta} `))
+// routes
+app.get('/', (req, res) => {
+  let search = req.query.job;
+  let query = '%' + search + '%';
 
-//db connection
-db
-.authenticate()
-.then( () => console.log("conectou ao banco com sucesso"))
-.catch((err)=> console.log("ocorreu um erro ao conectar ",err))
-
-//routes
-app.get('/', (req,res)=> {
-
-    let search = req.query.job
-    let query = '%' +search+ '%'
-
-
-    if(!search){
-
-    Job.findAll({order: [
-        ['createdAt','DESC']
-    ]})
-    .then(jobs => {
-        res.render('index', {
-            jobs
-        })
-    })
-    .catch(err=>console.log(err))
-}else{
+  if (!search) {
     Job.findAll({
-        where: {title: {[Op.like]:query}},
-        order: [
-        ['createdAt','DESC']
-    ]})
-    .then(jobs =>{
-        res.render('index', {
-            jobs, search
-        })
+      order: [['createdAt', 'DESC']]
     })
-    .catch(err=>console.log(err))
-}
+      .then(jobs => {
+        res.render('index', { jobs });
+      })
+      .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: { title: { [Op.like]: query } },
+      order: [['createdAt', 'DESC']]
+    })
+      .then(jobs => {
+        res.render('index', { jobs, search });
+      })
+      .catch(err => console.log(err));
+  }
+});
 
-})
+app.use('/jobs', require('./routes/jobs'));
 
-app.use('/jobs', require('./routes/jobs'))
-
+// Start server
+app.listen(PORT, () => console.log(`O Express está rodando na porta ${PORT}`));
